ecd3b287119e7857224621f623bb06220e08d295
MeutiaArafah <meutiaarafah@gmail.com>
2025-02-27 10:51:30 +0700

    update percobaan 2 pertanyaan 1

--- jobsheet3/Mahasiswa08.java
+++ jobsheet3/Mahasiswa08.java
@@ -4,4 +4,10 @@ public class Mahasiswa08 {
     public String kelas;
     public float ipk;
 
+    void cetakInfo(){
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : "+ nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk );
+    }
 }
