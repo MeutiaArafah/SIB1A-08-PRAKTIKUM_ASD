31b05654498ddfd29f0d71d6c19d5c8db2653b82
MeutiaArafah <meutiaarafah@gmail.com>
2025-03-20 08:42:32 +0700

    insertion sort

--- jobsheet6/SortingMain08.java
+++ jobsheet6/SortingMain08.java
@@ -2,9 +2,11 @@ public class SortingMain08 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
         Sorting08 dataurut1 = new Sorting08(a, a.length);
         Sorting08 dataurut2 = new Sorting08(b, b.length);
+        Sorting08 dataurut3 = new Sorting08(c, c.length);
 
         // bubble sort
         System.out.println("Data awal 1");
@@ -20,5 +22,13 @@ public class SortingMain08 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        // insertion sort
+        System.out.println();
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
