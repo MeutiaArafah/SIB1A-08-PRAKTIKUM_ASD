fef978403e25f469c441e309ac500f0a795964e4
MeutiaArafah <meutiaarafah@gmail.com>
2025-05-22 09:45:26 +0700

    percobaan 1

---
+++ jobsheet12/dll/DoubleLinkedLists01.java
@@ -0,0 +1,86 @@
+package dll;
+
+public class DoubleLinkedLists01 {
+    Node01 head;
+    Node01 tail;
+
+    public DoubleLinkedLists01() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa01 data) {
+        Node01 newNode = new Node01(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa01 data) {
+        Node01 newNode = new Node01(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // menambahkan node pada data key
+    public void insertAfter(String keyNim, Mahasiswa01 data) {
+        Node01 current = head;
+
+        // cari node dengan nim = keyNim
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("Node dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+        Node01 newNode = new Node01(data);
+
+        // Jika current adalah tail, cukup tambahkan di akhir
+        if (current == tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            // Sisipkan di tengah
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next = newNode;
+            current.next.prev = newNode;
+        }
+        System.out.println("Node berhasil disisipkan setelah NIM " + keyNim);
+    }
+    public void print(){
+        Node01 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+
+    public Node01 search(String nim){
+        Node01 current = head;
+
+        while (current != null) {
+            if (current.data.nim.equals(nim)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+
+    }
+}
---
+++ jobsheet12/dll/DoubleLinkedListsMain.java
@@ -0,0 +1,76 @@
+package dll;
+
+import java.util.Scanner;
+
+public class DoubleLinkedListsMain {
+    public static void main(String[] args) {
+        DoubleLinkedLists01 list = new DoubleLinkedLists01();
+        Scanner scan08 = new Scanner(System.in);
+        int pilihan;
+
+        do {
+            System.out.println("\nMenu Double Linked List Mahasiswa");
+            System.out.println("1. Tambah di awal");
+            System.out.println("2. Tambah di akhir");
+            System.out.println("3. Hapus dari awal");
+            System.out.println("4. Hapus dari akhir");
+            System.out.println("5. Tampilkan data");
+            System.out.println("6. Cari Mahasiswa berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = scan08.nextInt();
+            scan08.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa01 mhs = inputMahasiswa(scan08);
+                    list.addFirst(mhs);
+                    break;
+                case 2:
+                    Mahasiswa01 mhs1 = inputMahasiswa(scan08);
+                    list.addLast(mhs1);
+                    break;
+                case 3:
+                    // list.removeFirst();
+                    break;
+                case 4:
+                    // list.removeLast();
+                    break;
+                case 5:
+                    list.print();
+                    break;
+                case 6:
+                    System.out.print("Masukkan NIM yang dicari: ");
+                    String nim = scan08.nextLine();
+                    Node01 found = list.search(nim);
+                    if (found != null) {
+                        System.out.println("Data ditemukan:");
+                        found.data.tampil();
+                    } else {
+                        System.out.println("Data tidak ditemukan.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Keluar dari program.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid!");
+                    break;
+            }
+        } while (pilihan != 0);
+
+    }
+
+    public static Mahasiswa01 inputMahasiswa(Scanner scan08) {
+        System.out.print("NIM     : ");
+        String nim = scan08.nextLine();
+        System.out.print("Nama    : ");
+        String nama = scan08.nextLine();
+        System.out.print("Kelas   : ");
+        String kelas = scan08.nextLine();
+        System.out.print("IPK     : ");
+        double ipk = scan08.nextDouble();
+        scan08.nextLine();
+        return new Mahasiswa01(nim, nama, kelas, ipk);
+    }
+}
